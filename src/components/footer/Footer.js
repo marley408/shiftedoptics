f73d98e312aef6276212d7fcb2d9fcc9843a7d21
Matthew <mattsjsu14@gmail.com>
2020-04-19 13:41:19 -0700

    added social media icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import './Footer.css' 
+import React from "react"
+import "./Footer.css"
+import GrayInstagram from "./GrayInstagram"
+import GrayYoutube from "./GrayYoutube"
 
 const Footer = () => {
   return (
     <footer>
-      Designed, developed, and deployed by <a href='https://www.developedbymatt.com' target='_blank' rel='noopener noreferrer'>Matthew </a> &amp; Wesley Chang. 
+      <ul>
+        <li>
+          <a
+            href="https://www.instagram.com/shiftedoptics/"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Shifted Optics Instagram"
+          >
+            <GrayInstagram />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCWlvFe9d0YZhunGxOw17W4g"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Shifted Optics YouTube Channel"
+          >
+            <GrayYoutube />
+          </a>
+        </li>
+      </ul>
+      <p>
+        Designed, developed, and deployed by{" "}
+        <a
+          href="https://www.developedbymatt.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Matthew{" "}
+        </a>{" "}
+        &amp; Wesley Chang.
+      </p>
     </footer>
   )
 }
