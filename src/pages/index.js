c577daf138a1280b2eee0db215476523faa841f3
Matthew <mattsjsu14@gmail.com>
2020-04-19 13:41:53 -0700

    added contact section w/social media icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import React from "react"
 import PrimaryHero from "../components/hero/PrimaryHero"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Contact from '../components/contact/Contact'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <PrimaryHero />
-    
+    <Contact />
   </Layout>
 )
 
