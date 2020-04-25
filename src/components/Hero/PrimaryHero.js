7c5087b959c6de1c79b0134f2d2a28c8ac250dd5
Wesley Chang <weschang15@gmail.com>
2020-04-25 10:22:20 -0700

    fix: hero height on mobile and image quality

--- src/components/Hero/PrimaryHero.js
+++ src/components/Hero/PrimaryHero.js
@@ -1,39 +1,26 @@
-import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
 import React from "react"
 import styled from "styled-components"
+import HeroImage from "./HeroImage"
 
 const HeroContainer = styled.div`
   position: relative;
 `
 
 const HeroContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  text-align: center;
   color: var(--white);
+  left: 50%;
+  max-width: 1280px;
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+  width: 100%;
 `
 
 function PrimaryHero() {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "hero@3x.png" }) {
-        childImageSharp {
-          # Specify a fixed image and fragment.
-          # The default width is 400 pixels
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
   return (
     <HeroContainer>
-      <Img fluid={data.file.childImageSharp.fluid} />
+      <HeroImage />
       <HeroContent>
         <h1>Unique, Focused, and Compelling Content.</h1>
         <p>From the big picture to the details that matter.</p>
