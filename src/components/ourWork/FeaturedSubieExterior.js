abf013181d222df6cde8a691d5a9f4faf35f3267
Matthew <mattsjsu14@gmail.com>
2020-05-24 13:48:27 -0700

    added our work section

---
+++ src/components/ourWork/FeaturedSubieExterior.js
@@ -0,0 +1,24 @@
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import React from "react"
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "featured-subie-exterior.png" }) {
+        childImageSharp {
+          # Specify a fixed image and fragment.
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Img
+      fluid={data.file.childImageSharp.fluid}
+      alt="Gatsby Docs are awesome"
+    />
+  )
+}
