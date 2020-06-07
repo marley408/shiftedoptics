c48a3136b537b1830b1e5716c5f0d9a0f799c4d1
Wesley Chang <weschang15@gmail.com>
2020-06-07 12:58:12 -0700

    refactor: remove dead code

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,27 +5,16 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Footer from "./footer/Footer"
 import "./layout.css"
-import Header from "./navbar/navbar"
-import Footer from './footer/Footer'
+import NavBar from "./navbar/NavBar"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <NavBar />
       <div>
         <main>{children}</main>
         <Footer />
