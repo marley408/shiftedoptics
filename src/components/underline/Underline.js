5714453311ca66423b06826b363b2651531f3749
Wesley Chang <weschang15@gmail.com>
2020-06-08 13:28:53 -0700

    fix: restyle underline

--- src/components/underline/Underline.js
+++ src/components/underline/Underline.js
@@ -3,8 +3,8 @@ import styled from "styled-components"
 const Underline = styled.span`
   position: relative;
   z-index: 1;
-  background-image: linear-gradient(#e9a8ee, #e9a8ee);
-  background-size: 100% 0.45em;
+  background-image: linear-gradient(#d2d3d2, #d2d3d2);
+  background-size: 100% 0.1em;
   background-position: 0 bottom;
   background-repeat: no-repeat;
 `
