6d45392f78c213ef484007995e158b1ea94cc646
Wesley Chang <weschang15@gmail.com>
2020-06-13 13:57:47 -0700

    feat: add Sanity CMS to local repo

---
+++ content/schemas/post.js
@@ -0,0 +1,34 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Write a descriptive and unique name for this item.",
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+    },
+  },
+}
