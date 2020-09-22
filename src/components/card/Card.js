57c8944dddd31957a8c6d91556e6c2d654b9a8ce
Wesley Chang <weschang15@gmail.com>
2020-09-21 19:27:15 -0700

    fix: update box shadow style

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,7 +4,7 @@ const Card = styled.div`
   background-color: var(--white);
   padding: 1em;
   border-radius: 8px;
-  box-shadow: 0 4px 10px rgba(6, 5, 43, 0.25);
+  box-shadow: 0 8px 24px rgba(6, 5, 43, 0.15);
   h3 {
     margin: 0.75em 0 0.25em;
     font-size: 1.563em;
